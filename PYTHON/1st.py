99b4b9df98ca5adcec51dc3c246098534bfb6790
Shorya Sharma <hksharma8055@gmail.com>
2025-11-04 11:17:48 +0530

    First commit

---
+++ PYTHON/1st.py
@@ -0,0 +1,45 @@
+# a= int(input("enter value"))
+# b= int(input("enter value"))
+# c= a+b
+# print (c)
+# a,b,c,d,e=map(int,(input("enter value")).split())
+# a,b,c=(int,input("enter value: \n enter value2: \n enter value3:").split())
+# c=a+b 
+# print(c
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# temp = a
+# a = b
+# b = temp
+
+# print("After swapping:")
+# print("a =", a)
+# print("b =", b)
+
+
+
+
+# a =  10
+# b = 20
+# c = 20
+# d= b==c
+# print(d)
+
+# a=1
+# b=3
+# print (a+b)
+# print (a-b)
+# print (a*b)
+# print (a/b)
+
+#progrmae for even odd 
+# programe for leap year
+
+
+year = int(input("Enter a year: "))
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print(year, "is a leap year.")
+else:
+    print(year, "is not a leap year.")
